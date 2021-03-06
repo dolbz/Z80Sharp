df6f304aca8a886b50dab208a6a72dbea07b6bcb
Nathan Randle <nrandle@dolbz.com>
2021-03-06 00:12:53 +0000

    Add copyright header to each .cs file

--- Z80.Tests/SetCarryFlagTest.cs
+++ Z80.Tests/SetCarryFlagTest.cs
@@ -1,3 +1,7 @@
+//  
+// Copyright (c) 2021, Nathan Randle <nrandle@dolbz.com>. All rights reserved.  
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.  
+// 
 using System.Runtime.Serialization;
 using NUnit.Framework;
 
--- Z80/Instructions/RETN.cs
+++ Z80/Instructions/RETN.cs
@@ -1,3 +1,7 @@
+//  
+// Copyright (c) 2021, Nathan Randle <nrandle@dolbz.com>. All rights reserved.  
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.  
+// 
 namespace Z80.Instructions {
     internal class RETN : RET {
         public RETN(Z80Cpu cpu) : base(cpu, JumpCondition.Unconditional) {
